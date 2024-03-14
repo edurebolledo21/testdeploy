2f1183c9847b6401433ffc2917c706820a675d2d
edurebolledo21 <edurebolledo68@gmail.com>
2024-03-14 16:29:33 -0400

    update-tailwind

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <form action="">
-      <input type="text" name="nombre" />
-      <label for="nombre">name</label>
-      <textarea name="write" id="write" cols="30" rows="10"></textarea>
-      <label for="write">what's problem?</label>
-      <button type="submit">send</button>
+    <form class="flex flex-col" action="">
+      <div class="flex flex-row">
+        <input type="text" name="nombre" />
+        <label for="nombre">name</label>
+      </div>
+      <div class="flex flex-row">
+        <textarea name="write" id="write" cols="30" rows="10"></textarea>
+        <label for="write">what's problem?</label>
+      </div>
+      <button class="bg-slate-500 w-16" type="submit">send</button>
     </form>
     <br />
-    <NuxtLink to="/">HOME</NuxtLink>
+    <NuxtLink class="text-red-700" to="/">HOME</NuxtLink>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <h1 class="text-3xl font-bold text-cyan-200 underline">Hello world</h1>
     <br />
     <ul>
       <li><NuxtLink to="contacto">CONTACTO</NuxtLink></li>
