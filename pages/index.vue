1ce50a0264576010adcb12b93929b53bb210e499
edurebolledo21 <edurebolledo68@gmail.com>
2024-03-15 15:39:41 -0400

    update tailwind css

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <h1 class="text-3xl font-bold text-cyan-200 underline">Hello world</h1>
     <br />
     <ul>
-      <li><NuxtLink to="contacto">CONTACTO</NuxtLink></li>
-      <li><NuxtLink to="servicios">SERVICIOS</NuxtLink></li>
+      <li><NuxtLink to="contacto">CONTACT</NuxtLink></li>
+      <li><NuxtLink to="servicios">SERVICES</NuxtLink></li>
     </ul>
   </div>
 </template>
