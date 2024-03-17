6a7a5ed0f77feee4e9f53eadd24a603c5b9e2d9e
edurebolledo21 <edurebolledo68@gmail.com>
2024-03-17 02:29:49 -0400

    updateBuildGithub

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <form class="flex flex-col" action="">
-      <div class="flex flex-row">
-        <input type="text" name="nombre" />
+    <NuxtLink to="/">
+      <h1
+        class="text-xl p-4 text-fuchsia-500 hover:text-slate-900 transition-all duration-300"
+      >
+        Hello Word
+      </h1>
+    </NuxtLink>
+    <form class="px-4 flex flex-col gap-4 h-full" action="">
+      <div class="flex flex-row gap-4">
+        <input
+          class="border-2 rounded outline-none border-black"
+          type="text"
+          name="nombre"
+        />
         <label for="nombre">name</label>
       </div>
-      <div class="flex flex-row">
-        <textarea name="write" id="write" cols="30" rows="10"></textarea>
+      <div class="flex flex-row gap-4">
+        <textarea
+          class="border-2 rounded outline-none border-black"
+          name="write"
+          id="write"
+          cols="30"
+          rows="10"
+        ></textarea>
         <label for="write">what's problem?</label>
       </div>
-      <button class="bg-slate-500 w-16" type="submit">send</button>
+      <button
+        class="hover:bg-slate-500 transition-all duration-500 rounded-md w-16"
+        type="submit"
+      >
+        send
+      </button>
     </form>
     <br />
     <NuxtLink class="text-red-700" to="/">HOME</NuxtLink>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold text-cyan-200 underline">Hello world</h1>
+    <h1
+      class="hover:text-red-500 cursor-pointer transition-all duration-300 text-3xl font-bold text-cyan-200 underline"
+    >
+      Hello world
+    </h1>
     <br />
     <ul>
       <li><NuxtLink to="contacto">CONTACT</NuxtLink></li>
